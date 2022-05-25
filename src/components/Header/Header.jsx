9b92775a5e6775382326399b816dd6f7ee520816
md-alif10000 <md.alif10000@gmail.com>
2022-05-26 00:09:06 +0600

    ui update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Button, Drawer, List, ListItem, Stack, Link } from "@mui/material";
 import WidgetsIcon from "@mui/icons-material/Widgets";
 import { Link as RouterLink } from "react-router-dom";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
 
 const Header = () => {
   const [drawerOpen, setdrawerOpen] = useState(false);
@@ -16,20 +16,19 @@ const Header = () => {
         onClose={() => setdrawerOpen(false)}
       >
         <div className="sideDrawer">
-          <div className="drawerClose" onClick={()=>setdrawerOpen(false)} >
-
-            <CloseIcon/>
+          <div className="drawerClose" onClick={() => setdrawerOpen(false)}>
+            <CloseIcon />
           </div>
           <div className="logo">
-          <img src="./images/logo.svg" alt="" />
+            <img src="./images/logo.svg" alt="" />
           </div>
           <div className="links">
-            <List 
-             sx={{
-              width: '100%',
-              fontSize:"20"
-         
-            }}>
+            <List
+              sx={{
+                width: "100%",
+                fontSize: "20",
+              }}
+            >
               <ListItem>
                 <Link href="/" underline="none">
                   Home
@@ -81,11 +80,9 @@ const Header = () => {
           <Stack spacing={2} direction="row">
             <Button href="/login" variant="contained">
               <span className="btnText">Login</span>
-              
             </Button>
             <Button href="/sign-up" variant="contained">
-            <span className="btnText">Sign up</span>
-          
+              <span className="btnText">Sign up</span>
             </Button>
           </Stack>
         </div>
